82fe45afbd18385e8a682a55b574e265596f7abe
makeyouhigh <73630857+makeyouhigh@users.noreply.github.com>
2025-05-15 16:10:54 +0900

    Update screen.c

--- boards/shields/nice_view_gem/widgets/screen.c
+++ boards/shields/nice_view_gem/widgets/screen.c
@@ -111,12 +111,13 @@ static struct battery_status_state battery_status_get_state(const zmk_event_t *e
     // ② Peripheral 배터리 처리 (충전 정보 없음)
 #if IS_ENABLED(CONFIG_ZMK_SPLIT_BLE_CENTRAL_BATTERY_LEVEL_PROXY)
     static uint8_t peripheral_level = 0;
+    static bool peripheral_charging = false;
     {
-        /* 변수 선언을 if 밖으로 분리 */
         const struct zmk_peripheral_battery_state_changed *pev;
         pev = as_zmk_peripheral_battery_state_changed(eh);
         if (pev) {
             peripheral_level = pev->state_of_charge;
+            peripheral_charging = pev->charging;
         }
     }
 #else
@@ -129,7 +130,7 @@ static struct battery_status_state battery_status_get_state(const zmk_event_t *e
         .usb_present         = usb_present,
 #if IS_ENABLED(CONFIG_ZMK_SPLIT_BLE_CENTRAL_BATTERY_LEVEL_PROXY)
         .peripheral_level    = peripheral_level,
-        .peripheral_charging = false,
+        .peripheral_charging = peripheral_charging,
 #endif
     };
 }
