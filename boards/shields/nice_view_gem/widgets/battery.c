bce1f42c85d1a6a4483ab174e263fa9de4503626
makeyouhigh <73630857+makeyouhigh@users.noreply.github.com>
2025-05-15 15:53:19 +0900

    Update battery.c

--- boards/shields/nice_view_gem/widgets/battery.c
+++ boards/shields/nice_view_gem/widgets/battery.c
@@ -4,6 +4,79 @@
 
 LV_IMG_DECLARE(bolt);
 
+// 중앙 배터리 표시
+static void draw_level(lv_obj_t *canvas, const struct status_state *state) {
+    lv_draw_label_dsc_t dsc;
+    init_label_dsc(&dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_RIGHT);
+
+    char text[10] = {};
+    sprintf(text, "%i%%", state->battery);
+    lv_canvas_draw_text(canvas, 26, 19, 42, &dsc, text);
+}
+
+static void draw_charging_level(lv_obj_t *canvas, const struct status_state *state) {
+    lv_draw_img_dsc_t img_dsc;
+    lv_draw_img_dsc_init(&img_dsc);
+    lv_draw_label_dsc_t dsc;
+    init_label_dsc(&dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_RIGHT);
+
+    char text[10] = {};
+    sprintf(text, "%i%%", state->battery);
+    lv_canvas_draw_text(canvas, 26, 19, 35, &dsc, text);
+    lv_canvas_draw_img(canvas, 62, 21, &bolt, &img_dsc);
+}
+
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_BLE_CENTRAL_BATTERY_LEVEL_PROXY)
+// 주변 배터리 표시
+static void draw_peripheral_level(lv_obj_t *canvas, const struct status_state *state) {
+    lv_draw_label_dsc_t dsc;
+    init_label_dsc(&dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_RIGHT);
+
+    char text[10] = {};
+    sprintf(text, "%i%%", state->peripheral_battery);
+    lv_canvas_draw_text(canvas, 26, 30, 42, &dsc, text);
+}
+
+static void draw_peripheral_charging_level(lv_obj_t *canvas, const struct status_state *state) {
+    lv_draw_img_dsc_t img_dsc;
+    lv_draw_img_dsc_init(&img_dsc);
+    lv_draw_label_dsc_t dsc;
+    init_label_dsc(&dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_RIGHT);
+
+    char text[10] = {};
+    sprintf(text, "%i%%", state->peripheral_battery);
+    lv_canvas_draw_text(canvas, 26, 30, 35, &dsc, text);
+    lv_canvas_draw_img(canvas, 62, 32, &bolt, &img_dsc);
+}
+#endif
+
+// 공통 상태에 따라 분기
+void draw_battery_status(lv_obj_t *canvas, const struct status_state *state) {
+    lv_draw_label_dsc_t label_left_dsc;
+    init_label_dsc(&label_left_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_LEFT);
+
+    // 중앙 표시
+    lv_canvas_draw_text(canvas, 0, 19, 25, &label_left_dsc, "BAT");
+    if (state->charging) {
+        draw_charging_level(canvas, state);
+    } else {
+        draw_level(canvas, state);
+    }
+
+    // 주변 표시
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_BLE_CENTRAL_BATTERY_LEVEL_PROXY)
+    lv_canvas_draw_text(canvas, 0, 30, 25, &label_left_dsc, "BAT");
+    if (state->peripheral_charging) {
+        draw_peripheral_charging_level(canvas, state);
+    } else {
+        draw_peripheral_level(canvas, state);
+    }
+#endif
+}
+
+
+/*
+
 static void draw_level(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_label_dsc_t label_right_dsc;
     init_label_dsc(&label_right_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_RIGHT);
@@ -61,7 +134,7 @@ void draw_battery_status(lv_obj_t *canvas, const struct status_state *state) {
     draw_peripheral_level(canvas, state);
 #endif
 }
-
+*/
 
 
 /*
